2ba86f61b7471601f3edb4e716d508105839afee
ihommani <ismaelhommani@gmail.com>
2016-05-26 16:43:12 +0200

    Add jdk 8 specific instructions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created on 26/05/16.<br/>
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println(sayHello());
+    }
+
+
+    private static String sayHello() {
+        List<String> hellos = Collections.emptyList();
+        hellos.add("Hello");
+        hellos.add("Hello");
+        hellos.add("Hello");
+        hellos.add("Hello");
+        hellos.add("Hello");
+
+        return hellos.stream().reduce((s, s2) -> s + s2).get();
     }
 }
