1c46bb05cd5c9166268854bb841366475637e24e
ihommani <ismaelhommani@gmail.com>
2016-05-26 15:18:42 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created on 26/05/16.<br/>
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created on 26/05/16.<br/>
+ */
+public class MainTest {
+
+    @Test
+    public void dumb_test() {
+        Assert.assertEquals(true, true);
+    }
+}
